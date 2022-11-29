e7d1b6900c94e7203d2d833410b1e53800d9a9bc
Sho konosu <s21C1044AX@s.chibakoudai.jp>
2022-11-29 22:16:58 +0900

    新たに作成したプログラム

--- Newtest.bash
+++ Newtest.bash
@@ -4,15 +4,8 @@
 #1/0test#
 
 ### I/O TEST ###
-ng () {
-      echo NG at Line $1
-      res=1
-}
-
-res = 0
 
 out=$(./calculate)
 [ "${out}" = 56, 1.0, 720 ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK
-exit $res
+exit $out
