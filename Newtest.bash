0ee1dfcf3453a0d37cda4449116a136861a4cc60
Sho konosu <s21C1044AX@s.chibakoudai.jp>
2022-11-29 21:51:16 +0900

    新たに作成したプログラム

--- Newtest.bash
+++ Newtest.bash
@@ -1,14 +1,28 @@
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Sho Konosu
 # SPDX-License-Identifier: BSD-3-Clause
+#1/0test#
 
-ng () 
+ng () {
+echo NG at line $1
+res=1
+}
 
 res=0
 
-#1/0test#
+### I/O TEST ###
 out=$(./calculate)
-[ "${out}" = "56, 1.0, 720" ] || ng ${LINENO}
+[ "${out}" = 56, 1.0, 720 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}	    
+[ "${out}" = "" ] || ng ${LINENO}
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
