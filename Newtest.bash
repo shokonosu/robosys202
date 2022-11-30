30e176ce40920b3a28b24c3806607741be90771f
Sho konosu <s21C1044AX@s.chibakoudai.jp>
2022-11-30 15:23:34 +0900

    新たに作成したプログラム

--- Newtest.bash
+++ Newtest.bash
@@ -3,13 +3,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-res=0
+      res=0
 }
 
 
 ### 1/0 TEST ###
 out=$(./calculate)
-[ "${out}" = 56, 1.0, 720 ] || ng ${LINENO}
+[ "${out}" = "56 1.0 720" ] || ng ${LINENO}
 
-["$res" = 0] && echo OK
+[ "$res" = 0 ] && echo OK
 exit $res
