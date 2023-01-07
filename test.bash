560e1532629227b15a067362125f214a83d02b94
Sho konosu <s21C1044AX@s.chibakoudai.jp>
2023-01-07 15:44:05 +0900

    a

--- test.bash
+++ test.bash
@@ -26,6 +26,10 @@ out=$(echo A | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
+out=$(echo a | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 
 [ "$res" = 0 ] && echo OK
    exit $res
